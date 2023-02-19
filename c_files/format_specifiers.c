a213a70c23f862b127ad4b9c337e4448c255bae8
stephenkiai <stephenkiai23@gmail.com>
2023-02-19 11:28:03 +0300

    format_specifiers

--- c_files/format_specifiers.c
+++ c_files/format_specifiers.c
@@ -14,10 +14,16 @@ int main(){
 float item1 = 5.546;
 float item2 = 10.834;
 float item3 = 100.999;
+float item4 = 20.863;  
+float item5 = 34.237;
+float item6 = 678.456;
 {
 printf("item 1: kshs.%6.3f\n", item1);
 printf("item 2: kshs.%6.3f\n", item2);
 printf("item 3: kshs.%6.3f\n", item3);
+printf("item 4: kshs.%f\n", item4); /*no format to 6-to right align and .3 decimal points*/
+printf("item 5: kshs.%f\n", item5); /*same case*/
+printf("item 6: kshs.%f\n", item6); /*same case*/
 }
 
 return 0;

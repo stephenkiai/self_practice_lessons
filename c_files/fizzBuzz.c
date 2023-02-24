90416598d723ff4c928c98cd708eb735375cb7ac
stephenkiai <123061554+stephenkiai@users.noreply.github.com>
2023-02-25 01:59:50 +0300

    Update fizzBuzz.c

--- c_files/fizzBuzz.c
+++ c_files/fizzBuzz.c
@@ -14,10 +14,6 @@ int main()
     int i;
     for (i = 1; i <= 100; i++)
     {
-        /*if (i == 100)*/
-        /*{*/
-          /*  printf("Buzz ");*/
-        /*}*/
         if (i % 3 == 0 && i % 5 != 0)
         {
             printf("Fizz ");
